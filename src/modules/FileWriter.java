9c3ac68d0e9893d77179ce08a227ec81a3faf824
Benjamin Hsu <benjaminhsu99@gmail.com>
2021-01-31 23:42:21 -0500

    Added empty (TODO) FileWriter and LogWriter class files.

---
+++ src/modules/FileWriter.java
@@ -0,0 +1,18 @@
+/*
+CNT 5105C "Computer Networks" - Spring 2021
+Group Project
+Lavanya Khular, Chitranshu Raj, Benjamin Hsu
+
+Last Edited: 2/1/2021
+*/
+
+//Created package to group together all the various .java files together
+package src.modules;
+
+//Imports
+import java.io.*; //Exceptions, File, BufferedReader
+
+public class FileWriter
+{
+    //TODO
+}
